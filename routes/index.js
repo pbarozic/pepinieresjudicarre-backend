dc0a6d57b5d76a559eda5dac68003d4f29b87829
Pierre-Baptiste <pierrebaptiste.rozic@gmail.com>
2023-09-08 09:56:44 +0200

    route get all

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,14 @@ require('../models/connection');
 const Variete = require('../models/varietes');
 
 
+// GET all varieties
+router.get('/', (req, res) => {
+  Variete.find() // Ceci récupérera toutes les plantes
+    .then(data => res.json({ data }))
+    .catch(error => res.status(500).json({ error }));
+});
+
+
 // GET varieties by 'famille'
 router.get('/:famille', (req, res) => {
   const famille = req.params.famille;
