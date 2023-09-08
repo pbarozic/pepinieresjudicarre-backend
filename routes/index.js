125a55f58d29a2716bbb8d43e3e8c07f020d05af
Pierre-Baptiste <pierrebaptiste.rozic@gmail.com>
2023-09-08 10:02:07 +0200

    fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 require('../models/connection');
 const Variete = require('../models/varietes');
 
-
 // GET all varieties
 router.get('/', (req, res) => {
   Variete.find() // Ceci récupérera toutes les plantes
@@ -12,14 +11,21 @@ router.get('/', (req, res) => {
     .catch(error => res.status(500).json({ error }));
 });
 
-
 // GET varieties by 'famille'
 router.get('/:famille', (req, res) => {
   const famille = req.params.famille;
 
-  Variete.find({ famille }) // This will filter by the 'famille' field
-    .then(data => res.json({ data }))
-    .catch(error => res.status(500).json({ error }));
+  if (famille === 'All' || famille === 'Nouveautés') {
+    // Si la famille est "All" ou "Nouveautés", renvoie toutes les variétés
+    Variete.find()
+      .then(data => res.json({ data }))
+      .catch(error => res.status(500).json({ error }));
+  } else {
+    // Sinon, filtre par la famille spécifiée
+    Variete.find({ famille })
+      .then(data => res.json({ data }))
+      .catch(error => res.status(500).json({ error }));
+  }
 });
 
 module.exports = router;
